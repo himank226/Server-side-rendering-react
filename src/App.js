d308ad0729174ac4d6c2e13ea75741c973999081
Himank Gupta <himank2602gupta@gmail.com>
2019-04-09 12:15:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
 
-          Drivezy
+          Server side rendering demo
         </header>
       </div>
     );
